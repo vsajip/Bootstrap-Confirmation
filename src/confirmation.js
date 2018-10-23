bc05286343af11283d77f4b7d60903ded02ad1bb
Владимир <ulladimil@gmail.com>
2018-10-23 13:51:11 +0300

    Unsubscribe for body click event (#95)

--- src/confirmation.js
+++ src/confirmation.js
@@ -304,7 +304,7 @@ class Confirmation extends Popover {
               return $(this).data(DATA_KEY) !== undefined;
             }).confirmation('hide');
 
-            $('body').off(`${Event.SHOWN}.${self.uid}`);
+            $('body').off(`${Event.CLICK}.${self.uid}`);
             self.eventBody = false;
           });
         }
